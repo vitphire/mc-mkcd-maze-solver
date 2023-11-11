d90dab5a097897bd73e9912af02520fc76d2fe71
Riba Dániel <61618352+vitphire@users.noreply.github.com>
2023-11-11 18:08:37 +0100

    Added find block with dummy content

--- main.ts
+++ main.ts
@@ -1,7 +1,22 @@
-//% color="#202020" weight=100
 namespace mazes {
-    //% block
-    export function helloWorld() {
-
+    //% block="find $block closest to $o || closer than $r blocks"
+    //% block.shadow=minecraftBlock
+    //% r.defl=10
+    //% o.shadow=minecraftCreatePosition
+    //% expandableArgumentMode="toggle"
+    export function findBlock(block: number, o: Position, r: number = 12): Position {
+        let found = []
+        for (let x = -r; x < r; x++) {
+            for (let y = -r; y < r; y++) {
+                for (let z = -r; z < r; z++) {
+                    if (blocks.testForBlock(block, o.add(pos(x,y,z)))) {
+                        found.push(o)
+                    }
+                }
+            }
+        }
+        player.say(found)
+        return o
     }
 }
+agent.teleport(mazes.findBlock(GRASS, agent.getPosition()), WEST)
